85476a7f339b667c97c72f5c27ec159cd64006ca
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-02-25 22:26:01 -0800

    Fixed Console error and broke assignment down into sections.

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log ("Button has been clicked!");
+//1. prompt the user for password criteria
+    // a. Password length 8 < 128 
+    // b. lowercase, uppercase, numbers and special characters
+
+// 2. Validate the input
+
+// 3. Generate password based on criteria
+
+// 4. Display password on the page.
+
+
+
+
+  return "Generated password will go here.";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
-
-function generatePassword() {
-  let pass = '';
-  let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$";
-}
-
-for (let i = 1; i <=8; i++) {
-  let char = Math.floor(math.random() * string.length + 1);
-password += string.charAt(char)
-}
-
-return {
-  password,
-}
-
-console.log(generatePassword());
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-addEventListener("click")
