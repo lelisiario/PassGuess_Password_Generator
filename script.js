7c4727bad1ab622dd17b31fd50020e486667fc7d
Lauren Elisiario <lauren.elisiario@gmail.com>
2024-02-25 17:47:44 -0800

    Started writing the code needed to make this  functional.

--- script.js
+++ script.js
@@ -10,5 +10,24 @@ function writePassword() {
 
 }
 
+
+function generatePassword() {
+  let pass = '';
+  let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$";
+}
+
+for (let i = 1; i <=8; i++) {
+  let char = Math.floor(math.random() * string.length + 1);
+password += string.charAt(char)
+}
+
+return {
+  password,
+}
+
+console.log(generatePassword());
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+addEventListener("click")
